068addb2dc2462286b2f197a673af47645a1d219
nanasi-1 <test_email>
2024-02-27 16:04:13 +0900

    pip関数の実装とか

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
+/**
+ * ピクチャーインピクチャーにする関数
+ * @param {HTMLIFrameElement} elem 
+ */
+async function pip(elem) {
+    if(documentPictureInPicture.window) return;
+    const container = elem.parentElement; // 復帰用
+    const pip = await documentPictureInPicture.requestWindow({
+        width: 500,
+        height: 1200,
+    });
+    pip.document.body.append(elem);
+
+    pip.addEventListener("unload", (event) => {
+        container.append(elem);
+    });
+    return pip;
+}
+
+window.addEventListener('urlChange-pip', async () => {
+
+    await sleep(500);
+    for (let i = 0; i < 20; i++) {
+        if(appendBtn(pip)) break;
+        await sleep(50);
+    }
+});
+
+/** ボタンを追加する関数 @param {() => any} handle  */
+async function appendBtn(handle) {
+    /** @type {Document} */
+    const doc = document.querySelector('[aria-label="教材モーダル"]>iframe').contentDocument;
+    const header = doc.querySelector('header');
+    if(!header) return false;
+    const isBookmark = !!header.querySelector('#bookmark-btn');
+
+    const btn = strToElement('<button id="pip-btn" class="u-button type-primary"></button>');
+    btn.style.position = 'absolute';
+    btn.style.right = `${ isBookmark ? 270 : 130}px`;
+    btn.style.top = '0';
+    btn.style.marginTop = '-10px';
+    btn.style.padding = '0';
+    btn.style.lineHeight = '42px';
+    btn.style.width = '130px';
+
+    btn.textContent = 'PiPで見る';
+    btn.addEventListener('click', handle);
+    header.insertBefore(btn, header.querySelector('button'));
+
+    return btn;
+}
+
 { // urlChangeイベント
     let oldUrl = ''; // URLの一時保管用
     const observer = new MutationObserver(async () => {
         await sleep(50); // 判定が速すぎたため
         if (oldUrl !== location.href) {
-            window.dispatchEvent(new CustomEvent('urlChange-n', { detail: oldUrl || '' }));
+            window.dispatchEvent(new CustomEvent('urlChange-pip', { detail: oldUrl || '' }));
             oldUrl = location.href; // oldUrlを更新
         }
     });
@@ -14,7 +66,7 @@
             attributes: true,
             characterData: true
         });
-        window.dispatchEvent(new CustomEvent('urlChange-n'));
+        window.dispatchEvent(new CustomEvent('urlChange-pip'));
     });
 }
 
@@ -22,4 +74,11 @@ function sleep(sec) {
     return new Promise(resolve => {
         setTimeout(() => { resolve(); }, sec);
     })
+}
+
+/** 文字列をHTMLElementにする関数 @param {String} str @returns {HTMLElement} */
+function strToElement(str, inHTML = false) {
+    const tempEl = document.createElement(inHTML ? 'html' : 'body');
+    tempEl.innerHTML = str;
+    return inHTML ? tempEl : tempEl.firstElementChild;
 }
